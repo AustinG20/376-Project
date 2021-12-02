242e6137373876a3b6bb3d38a4fe3ae9d7ce3bfb
Bero <34479277+bad-bird@users.noreply.github.com>
2021-12-02 03:23:30 -0500

    tuned

---
+++ 376_Project/Assets/Bero/CharacterAnimatorUpdated.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyControllerUpdated : MonoBehaviour
+{
+    public float lookRadius = 3;
+
+    Transform target;
+    public NavMeshAgent agent;
+    public fearfactor fearFactor;
+    public GameObject restPoint;
+    public bool followplayer;
+
+
+    public void Start()
+    {
+        followplayer = true;
+        target = PlayerManager.instance.player.transform;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    public void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius && followplayer)
+        {
+
+            agent.SetDestination(target.position);
+
+            if (distance <= agent.stoppingDistance)
+            {
+                // attack the target
+                fearFactor.scarred(1.0f);
+                followplayer = false;
+                //agent.SetDestination(restPoint.transform.position);
+                // face the target
+                FaceTarget();
+
+            }
+        }
+    }
+
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
